84111dc96dc47dbac587cc0095925540136e6ba2
alesha <aleshamulla2006@gmail.com>
2025-05-26 17:29:55 +0530

    Update config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// This file contains configuration settings for the Weather Now application
+const CONFIG = {
+    // API key for OpenWeatherMap
+    API_KEY: "3d96837bf6c59b37c98ef03b0571d838" // add your own API key here
+};
