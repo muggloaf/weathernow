6c4efaf6b128ce4eb5c9c6b8469024160cb9ec0b
alesha <aleshamulla2006@gmail.com>
2025-05-26 16:42:33 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// This file contains configuration settings for the Weather Now application
-// Replace this with your own OpenWeatherMap API key
-const CONFIG = {
-    API_KEY: "" //add your API key here
-};
